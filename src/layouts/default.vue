aa6dd878d7f8a6310a75f560019a5dbac9f9794e
evening97 <1204371664@qq.com>
2022-04-02 17:21:47 +0800

    第一次提交

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,3 +1,8 @@
 <template>
+  <Network
+    offlineTitle="断网处理标题"
+    desc="断网处理描述"
+    onlineTitle="网络连接成功????"
+  />
   <router-view />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import 'virtual:windi-base.css'
-import 'virtual:windi-components.css'
+import "virtual:windi-base.css";
+import "virtual:windi-components.css";
 // 你自定义的 css
-import './styles/main.css'
+import "./styles/main.css";
 
-import 'virtual:windi-utilities.css'
-import 'virtual:windi-devtools'
+import "virtual:windi-utilities.css";
+// import 'virtual:windi-devtools'
 
-import App from './App.vue'
-import { useModules } from 'virtual:modules'
+import App from "./App.vue";
+import { useModules } from "virtual:modules";
 
-const app = createApp(App)
+const app = createApp(App);
 
 // 插件自动加载
-useModules(app)
+useModules(app);
 
-app.mount('#app')
+app.mount("#app");
